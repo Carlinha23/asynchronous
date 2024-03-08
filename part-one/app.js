a7d6ffd83ce3369226dfcc037c3d1e2281e6ea3a
carla <moraescarla23@gmail.com>
2024-03-08 15:19:54 -0800

    asynchronous

--- part-one/app.js
+++ part-one/app.js
@@ -2,21 +2,30 @@ let favNumber = 5;
 let baseURL = "http://numbersapi.com";
 
 // 1.
-$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
-  console.log(data);
-});
+async function part1() {
+    let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
+    console.log(data);
+}
+part1();
+
 
 // 2.
 let favNumbers = [7, 11, 22];
-$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
-  console.log(data);
-});
+async function part2() {
+    let data = await $.getJSON(`${baseURL}/${favNumber}?json`);
+    console.log(data);
+}
+part1();
+
 
 // 3.
-Promise.all(
-  Array.from({ length: 4 }, () => {
-    return $.getJSON(`${baseURL}/${favNumber}?json`);
-  })
-).then(facts => {
-  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
-});
+async function part3() {
+    let facts = await Promise.all(
+      Array.from({ length: 4 }, () => $.getJSON(`${baseURL}/${favNumber}?json`))
+    );
+    facts.forEach(data => {
+      $('body').append(`<p>${data.text}</p>`);
+    });
+}
+part3();
+
